a37b73f080bc725de7e98a4a4b8e7cb428853fa6
Ryosuke Goto <ryosuke.goto@gmail.com>
2021-11-10 09:23:19 +0900

    Add interior/exterior calculation

--- src/App.svelte
+++ src/App.svelte
@@ -9,6 +9,13 @@
     bodyRatio,
     bodyStruct,
     bodyUnderground,
+    exteriorArea,
+    exteriorUnitPrice,
+    exteriorRatio,
+    interiorArea,
+    interiorUnitPrice,
+    interiorLevels,
+    interiorRatio,
   } from "./Data";
 
   export let project: string;
@@ -21,8 +28,16 @@
   let bodyUndergroundChoice: number = 0;
   let bodyAreaChoice: number;
   let bodyRatioChoice: number;
+  let exteriorGrade: string = "A";
+  let exteriorAreaChoice: number;
+  let exteriorRatioChoice: number;
+  let interiorGrade: string = "A";
+  let interiorAreaChoice: number;
+  let interiorRatioChoice: number;
+  let interiorLevelChoice: number;
 
-  $: totalPrice = (kasetsuPrice + bodyPrice) * totalArea;
+  $: totalPrice =
+    (kasetsuPrice + bodyPrice + exteriorPrice + interiorPrice) * totalArea;
 
   $: kasetsuPrice =
     Object.values(kasetsu)[kasetsuAreaChoice] *
@@ -34,13 +49,24 @@
     Object.values(bodyRatio)[bodyRatioChoice] *
     Object.values(bodyStruct)[bodyStructChoice] *
     Object.values(bodyUnderground)[bodyUndergroundChoice];
+
+  $: exteriorPrice =
+    (Object.values(exteriorArea[exteriorGrade])[exteriorAreaChoice] as number) *
+    Object.values(exteriorRatio)[exteriorRatioChoice] *
+    exteriorUnitPrice;
+
+  $: interiorPrice =
+    (Object.values(interiorArea[interiorGrade])[interiorAreaChoice] as number) *
+    Object.values(interiorRatio)[interiorRatioChoice] *
+    Object.values(interiorLevels)[interiorLevelChoice] *
+    interiorUnitPrice;
 </script>
 
 <main>
   <h1>概算見積</h1>
   <h2>{project}</h2>
 
-  <div class="radio-container">
+  <div class="container">
     <h3>概算計: {Math.ceil(totalPrice).toLocaleString()}円</h3>
     <label for="total-area" class="value">
       @ <input
@@ -67,7 +93,7 @@
 
   <Section title="躯体構造" price={bodyPrice} />
 
-  <div class="radio-container">
+  <div class="container">
     <p class="key">構造</p>
     {#each Object.keys(bodyStruct) as name, i}
       <label for={name}>
@@ -82,7 +108,7 @@
     <p class="value">{Object.values(bodyStruct)[bodyStructChoice]}円/㎡</p>
   </div>
 
-  <div class="radio-container">
+  <div class="container">
     <p class="key">地下階数</p>
     {#each Object.keys(bodyUnderground) as name, i}
       <label for={name}>
@@ -116,6 +142,78 @@
     <p class="value">x {Object.values(bodyRatio)[bodyRatioChoice]}</p>
     <Slider data={bodyRatio} bind:choice={bodyRatioChoice} />
   </div>
+
+  <Section title="外部仕上げ" price={exteriorPrice} />
+
+  <div class="container">
+    <p class="key">グレード</p>
+    {#each Object.keys(exteriorArea) as grade}
+      <label for={grade}>
+        <input
+          type="radio"
+          name="exteriorGrade"
+          value={grade}
+          bind:group={exteriorGrade}
+        />
+      </label>{grade}
+    {/each}
+  </div>
+
+  <div class="slider-container">
+    <p class="key">基準階面積</p>
+    <p class="value">
+      x {Object.values(exteriorArea[exteriorGrade])[exteriorAreaChoice]}
+    </p>
+    <Slider
+      data={exteriorArea[exteriorGrade]}
+      bind:choice={exteriorAreaChoice}
+    />
+  </div>
+
+  <div class="slider-container">
+    <p class="key">基準階平面比</p>
+    <p class="value">x {Object.values(exteriorRatio)[exteriorRatioChoice]}</p>
+    <Slider data={exteriorRatio} bind:choice={exteriorRatioChoice} />
+  </div>
+
+  <Section title="内部仕上げ" price={interiorPrice} />
+
+  <div class="container">
+    <p class="key">グレード</p>
+    {#each Object.keys(interiorArea) as grade}
+      <label for={grade}>
+        <input
+          type="radio"
+          name="interiorGrade"
+          value={grade}
+          bind:group={interiorGrade}
+        />
+      </label>{grade}
+    {/each}
+  </div>
+
+  <div class="slider-container">
+    <p class="key">基準階面積</p>
+    <p class="value">
+      x {Object.values(interiorArea[interiorGrade])[interiorAreaChoice]}
+    </p>
+    <Slider
+      data={interiorArea[interiorGrade]}
+      bind:choice={interiorAreaChoice}
+    />
+  </div>
+
+  <div class="slider-container">
+    <p class="key">基準階平面比</p>
+    <p class="value">x {Object.values(interiorRatio)[interiorRatioChoice]}</p>
+    <Slider data={interiorRatio} bind:choice={interiorRatioChoice} />
+  </div>
+
+  <div class="slider-container">
+    <p class="key">地上+地下階数</p>
+    <p class="value">x {Object.entries(interiorLevels)[interiorLevelChoice]}</p>
+    <Slider data={interiorLevels} bind:choice={interiorLevelChoice} />
+  </div>
 </main>
 
 <style>
@@ -138,20 +236,20 @@
     border: none;
     border-bottom: 1px solid black;
   }
-  .radio-container {
+  .container {
     margin: 5px auto;
     align-items: baseline;
     display: flex;
     width: 80%;
   }
 
-  .radio-container .key {
+  .container .key {
     width: 18%;
   }
-  .radio-container input {
+  .container input {
     margin-left: 10px;
   }
-  .radio-container .value {
+  .container .value {
     margin-left: auto;
   }
 
--- src/Data.ts
+++ src/Data.ts
@@ -6,6 +6,9 @@ export interface Coefficient {
   [grade: number | string]: number;
 }
 
+
+// 直接仮設
+
 export const kasetsu: Price = {
   [1000]: 13700,
   [2000]: 13000,
@@ -17,17 +20,19 @@ export const kasetsu: Price = {
 };
 
 export const kasetsuLevels: Coefficient = {
-  "3": 0.9,
-  "4": 0.92,
-  "5": 0.95,
-  "6": 0.97,
-  "7": 1.0,
-  "8": 1.02,
-  "9": 1.02,
-  "10": 1.1,
-  "12": 1.15,
-  "14": 1.2,
+  [3]: 0.9,
+  [4]: 0.92,
+  [5]: 0.95,
+  [6]: 0.97,
+  [7]: 1.0,
+  [8]: 1.02,
+  [9]: 1.02,
+  [10]: 1.1,
+  [12]: 1.15,
+  [14]: 1.2,
 };
+
+// 躯体
 
 export const bodyStruct: Price = {
   // 躯体構造
@@ -68,4 +73,144 @@ export const bodyRatio: Coefficient = {
   [6]: 1.11,
   [8]: 1.15,
   [10]: 1.19
+}
+
+
+// 土工・地業
+
+export const undergroundLevels: Price = {
+  // 地下階数
+  [0]: 26000,
+  [1]: 54400,
+  [2]: 50400,
+  [3]: 48000,
+}
+
+export const undergroundArea: Coefficient = {
+  // 地下階平均床面積
+  [1400]: 0.97,
+  [1200]: 0.98,
+  [1000]: 1.00,
+  [800]: 1.03,
+  [600]: 1.05,
+  [400]: 1.10
+}
+
+export const undergroundRatio: Coefficient = {
+  // 地下階平面細長比
+  [1]: 1.00,
+  [2]: 1.02,
+  [4]: 1.05,
+  [6]: 1.08,
+  [8]: 1.11,
+  [10]: 1.14
+}
+
+
+// 外部仕上げ 
+export const exteriorUnitPrice = 12000;
+
+const exteriorAreaA: Coefficient = {
+  // グレードA
+  [1400]: 1.45,
+  [1200]: 1.57,
+  [1000]: 1.72,
+  [800]: 1.93,
+  [600]: 2.23,
+  [400]: 2.72
+}
+
+const exteriorAreaB: Coefficient = {
+  // グレードa
+  [1400]: 0.84,
+  [1200]: 0.91,
+  [1000]: 1.00,
+  [800]: 1.12,
+  [600]: 1.30,
+  [400]: 1.58
+}
+
+const exteriorAreaC: Coefficient = {
+  // グレードA
+  [1400]: 0.74,
+  [1200]: 0.80,
+  [1000]: 0.88,
+  [800]: 0.99,
+  [600]: 1.14,
+  [400]: 1.39
+}
+
+export const exteriorArea = {
+  "A": exteriorAreaA,
+  "B": exteriorAreaB,
+  "C": exteriorAreaC
+}
+
+export const exteriorRatio: Coefficient = {
+  // 基準階平面形状比 
+  [1]: 1.00,
+  [2]: 1.00,
+  [4]: 1.05,
+  [6]: 1.11,
+  [8]: 1.11,
+  [10]: 1.11
+}
+
+
+// 内部仕上げ 
+export const interiorUnitPrice = 12000;
+
+const interiorAreaA: Coefficient = {
+  // グレードA
+  [1400]: 1.27,
+  [1200]: 1.28,
+  [1000]: 1.28,
+  [800]: 1.28,
+  [600]: 1.29,
+  [400]: 1.30
+}
+
+const interiorAreaB: Coefficient = {
+  // グレードa
+  [1400]: 0.99,
+  [1200]: 1.00,
+  [1000]: 1.00,
+  [800]: 1.01,
+  [600]: 1.02,
+  [400]: 1.04
+}
+
+const interiorAreaC: Coefficient = {
+  // グレードA
+  [1400]: 0.71,
+  [1200]: 0.72,
+  [1000]: 0.72,
+  [800]: 0.73,
+  [600]: 0.74,
+  [400]: 0.77
+}
+
+export const interiorArea = {
+  "A": interiorAreaA,
+  "B": interiorAreaB,
+  "C": interiorAreaC
+}
+
+export const interiorRatio: Coefficient = {
+  // 基準階平面形状比 
+  [1]: 1.00,
+  [2]: 1.00,
+  [4]: 1.05,
+  [6]: 1.11,
+  [8]: 1.11,
+  [10]: 1.11
+}
+
+export const interiorLevels: Coefficient = {
+  // 地上+地下階数
+  [7]: 0.98,
+  [9]: 1.00,
+  [10]: 1.01,
+  [12]: 1.03,
+  [14]: 1.05
 }
